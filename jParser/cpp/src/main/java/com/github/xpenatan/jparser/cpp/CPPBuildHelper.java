b9d58ff76a947973f1a8387a4a0d23d0c6a785a2
Natan <xpenatan@gmail.com>
2022-11-16 22:37:05 -0300

    testing

--- jParser/cpp/src/main/java/com/github/xpenatan/jparser/cpp/CPPBuildHelper.java
+++ jParser/cpp/src/main/java/com/github/xpenatan/jparser/cpp/CPPBuildHelper.java
@@ -137,7 +137,7 @@ public class CPPBuildHelper {
             buildConfig.sharedLibs[2] = libFolder;
         }
         mac64.cppFlags += " -std=c++11";
-        mac64.linkerFlags += " -rpath @loader_path";
+        mac64.linkerFlags += " -Wl,-rpath,'$$ORIGIN'";
         return mac64;
     }
 
