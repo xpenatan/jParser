8ad6e82b8d5f31c13076c29f6e0ff7546c07426a
Natan <xpenatan@gmail.com>
2022-11-16 21:04:29 -0300

    update loader

--- jParser/loader/src/main/java/com/github/xpenatan/jparser/loader/JParserLibraryLoader.java
+++ jParser/loader/src/main/java/com/github/xpenatan/jparser/loader/JParserLibraryLoader.java
@@ -19,19 +19,19 @@ public class JParserLibraryLoader {
         load(libraryName, null);
     }
 
-    public void load(String libraryName, String dependencyLibraryName) {
-        if(dependencyLibraryName != null) {
-            if(SharedLibraryLoader.isLinux) {
+    public void load(String libraryName01, String libraryName02) {
+        if(libraryName02 != null) {
+            if(SharedLibraryLoader.isLinux || SharedLibraryLoader.isMac) {
                 String dirName = "jparser-lib";
-                if(!SharedLibraryLoader.isLoaded(libraryName)) {
-                    String lib01Map = loader.mapLibraryName(libraryName);
-                    String lib02Map = loader.mapLibraryName(dependencyLibraryName);
+                if(!SharedLibraryLoader.isLoaded(libraryName01)) {
+                    String lib01Map = loader.mapLibraryName(libraryName01);
+                    String lib02Map = loader.mapLibraryName(libraryName02);
                     try {
                         String lib01Path = loader.extractFile(lib01Map, dirName).getParentFile().getAbsolutePath();
                         loader.extractFile(lib02Map, dirName);
                         String fullPath = lib01Path + File.separator + lib01Map;
                         System.load(fullPath);
-                        SharedLibraryLoader.setLoaded(libraryName);
+                        SharedLibraryLoader.setLoaded(libraryName01);
                     }
                     catch(Throwable ex) {
                         ex.printStackTrace();
@@ -39,12 +39,12 @@ public class JParserLibraryLoader {
                 }
             }
             else {
-                loader.load(dependencyLibraryName);
-                loader.load(libraryName);
+                loader.load(libraryName02);
+                loader.load(libraryName01);
             }
         }
         else {
-            loader.load(libraryName);
+            loader.load(libraryName01);
         }
     }
 }
