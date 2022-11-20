aea5acc8b68465a8c14b51229759120031701482
Natan <xpenatan@gmail.com>
2022-11-20 00:47:51 -0300

    Small loader improvement

--- jParser/loader/src/main/java/com/github/xpenatan/jparser/loader/JParserLibraryLoader.java
+++ jParser/loader/src/main/java/com/github/xpenatan/jparser/loader/JParserLibraryLoader.java
@@ -23,7 +23,9 @@ public class JParserLibraryLoader {
         if(libraryName02 != null) {
             if(SharedLibraryLoader.isLinux || SharedLibraryLoader.isMac || SharedLibraryLoader.isWindows) {
                 String dirName = "jparser-lib";
-                if(!SharedLibraryLoader.isLoaded(libraryName01)) {
+                boolean isLib01Loaded = SharedLibraryLoader.isLoaded(libraryName01);
+                boolean isLib02Loaded = SharedLibraryLoader.isLoaded(libraryName02);
+                if(!isLib01Loaded) {
                     String lib01Map = loader.mapLibraryName(libraryName01);
                     String lib02Map = loader.mapLibraryName(libraryName02);
                     try {
@@ -33,7 +35,10 @@ public class JParserLibraryLoader {
                         String fullPath01 = lib01Path + File.separator + lib01Map;
                         String fullPath02 = lib02Path + File.separator + lib02Map;
 
-                        System.load(fullPath02);
+                        if(!isLib02Loaded) {
+                            System.load(fullPath02);
+                            SharedLibraryLoader.setLoaded(libraryName02);
+                        }
                         System.load(fullPath01);
                         SharedLibraryLoader.setLoaded(libraryName01);
                     }
