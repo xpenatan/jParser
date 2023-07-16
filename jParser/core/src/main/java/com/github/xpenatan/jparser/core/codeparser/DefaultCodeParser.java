6169c3db12c318c656255a1943afec16a5c672a9
Natan <xpenatan@gmail.com>
2023-07-16 08:27:31 -0300

    update gradle

--- jParser/core/src/main/java/com/github/xpenatan/jparser/core/codeparser/DefaultCodeParser.java
+++ jParser/core/src/main/java/com/github/xpenatan/jparser/core/codeparser/DefaultCodeParser.java
@@ -6,7 +6,6 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.comments.BlockComment;
