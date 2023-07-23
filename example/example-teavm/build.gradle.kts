485a05a547d7e9f9c5a85fbbb6c6820f305f1f54
Natan <xpenatan@gmail.com>
2023-07-23 13:48:58 -0300

    update workflow

---
+++ example/example-teavm/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+}
+
+dependencies {
+    if(LibExt.exampleUseRepoLibs) {
+        testImplementation("com.github.xpenatan.jParser:jParser-loader:${LibExt.libVersion}")
+    }
+    else {
+        testImplementation(project(":jParser:loader"))
+    }
+    testImplementation(project(":example:example-core"))
+    testImplementation("junit:junit:${LibExt.jUnitVersion}")
+}
+
+tasks.named("clean") {
+    doFirst {
+        val srcPath = "$projectDir/src/main/"
+        project.delete(files(srcPath))
+    }
+}
+
+val tasksOrder = tasks.register<GradleBuild>("prepareTest") {
+    tasks = listOf(
+            ":example:example-build:clean",
+            ":example:example-core:clean",
+            ":example:example-build:generateNativeProject",
+            "compileTestJava"
+    )
+}
+
+tasks.named("test") {
+    dependsOn(tasksOrder)
+    mustRunAfter(tasksOrder)
+}
