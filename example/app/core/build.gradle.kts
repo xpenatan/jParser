ecbe3e741dcbfab767c920aab03ee96f722a768e
Natan <xpenatan@gmail.com>
2023-09-09 23:28:59 -0300

    attempt to fix build

--- example/app/core/build.gradle.kts
+++ example/app/core/build.gradle.kts
@@ -4,5 +4,5 @@ dependencies {
     implementation("com.badlogicgames.gdx:gdx:${LibExt.gdxVersion}")
 }
 
-tasks.build.get().dependsOn(":example:lib:generator:build_project")
-tasks.build.get().mustRunAfter(":example:lib:generator:build_project")
+tasks.compileJava.get().dependsOn(":example:lib:generator:build_project")
+tasks.compileJava.get().mustRunAfter(":example:lib:generator:build_project")
