ea6bdd18a55fbdf9c2eb786156a6fb524a327de6
Natan <xpenatan@gmail.com>
2023-09-09 23:07:21 -0300

    remove gradle code

--- example/app/core/build.gradle.kts
+++ example/app/core/build.gradle.kts
@@ -3,6 +3,3 @@ dependencies {
 
     implementation("com.badlogicgames.gdx:gdx:${LibExt.gdxVersion}")
 }
-
-tasks.build.get().dependsOn(":example:lib:generator:generateNativeProject")
-tasks.build.get().mustRunAfter(":example:lib:generator:generateNativeProject")
