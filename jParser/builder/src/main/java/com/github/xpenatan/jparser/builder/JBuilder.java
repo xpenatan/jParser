c3a26f36e4bf3116851f360856fd11dd8624ef37
Natan <xpenatan@gmail.com>
2023-10-14 18:12:36 -0300

    Change build to multi targets only

--- jParser/builder/src/main/java/com/github/xpenatan/jparser/builder/JBuilder.java
+++ jParser/builder/src/main/java/com/github/xpenatan/jparser/builder/JBuilder.java
@@ -4,25 +4,10 @@ import java.util.ArrayList;
 
 public class JBuilder {
 
-    public static void build(BuildConfig config, ArrayList<BuildTarget> targets) {
-        BuildTarget [] targetsArray = new BuildTarget[targets.size()];
-        for(int i = 0; i < targets.size(); i++) {
-            targetsArray[i] = targets.get(i);
-        }
+    public static void build(BuildConfig config, ArrayList<BuildMultiTarget> targets) {
+        BuildMultiTarget [] targetsArray = new BuildMultiTarget[targets.size()];
+        targets.toArray(targetsArray);
         build(config, targetsArray);
-    }
-
-    public static void build(BuildConfig config, BuildTarget ... targets) {
-        for(int i = 0; i < targets.length; i++) {
-            BuildTarget target = targets[i];
-            if(target != null) {
-                String targetName = target.getClass().getSimpleName();
-                System.out.println("##### Building: " + targetName + " #####");
-                if(!target.build(config)) {
-                    throw new RuntimeException();
-                }
-            }
-        }
     }
 
     public static void build(BuildConfig config, BuildMultiTarget ... targets) {
@@ -30,7 +15,11 @@ public class JBuilder {
             BuildMultiTarget target = targets[i];
             if(target != null) {
                 for(BuildTarget buildTarget : target.multiTarget) {
-                    build(config, buildTarget);
+                    String targetName = buildTarget.getClass().getSimpleName();
+                    System.out.println("##### Building: " + targetName + " #####");
+                    if(!buildTarget.build(config)) {
+                        throw new RuntimeException();
+                    }
                 }
             }
         }
