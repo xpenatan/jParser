7124792edec284cddbab62de2620dc1f88d15f96
Natan <xpenatan@gmail.com>
2024-01-11 08:43:29 -0300

    improve get version code

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,20 +1,36 @@
+import java.io.File
+import java.util.*
+
 object LibExt {
-    val libVersion: String = this.getVersion()
+    val libVersion: String = getVersion()
     const val jniGenVersion = "2.3.1"
     const val javaparserVersion = "3.24.7"
     const val jUnitVersion = "4.12"
     const val groupId = "com.github.xpenatan.jParser"
     const val exampleUseRepoLibs = false
     const val gdxVersion = "1.12.1"
-    const val teaVMVersion = "0.9.0"
+    const val teaVMVersion = "0.9.2"
 
-    private fun getVersion(): String {
-        var isRelease = System.getenv("RELEASE")
-        var libVersion = "1.0.0-SNAPSHOT"
-        if(isRelease != null && isRelease.toBoolean()) {
-            libVersion = "1.0.0-b4"
+}
+
+private fun getVersion(): String {
+    val isReleaseStr = System.getenv("RELEASE")
+    val isRelease = isReleaseStr != null && isReleaseStr.toBoolean()
+    var libVersion = "-SNAPSHOT"
+    val file = File("gradle.properties")
+    if(file.exists()) {
+        val properties = Properties()
+        properties.load(file.inputStream())
+        val version = properties.getProperty("version")
+        if(isRelease) {
+            libVersion = version
         }
-        System.out.println("jParser Version: " + libVersion)
-        return libVersion
     }
+    else {
+        if(isRelease) {
+            throw RuntimeException("properties should exist")
+        }
+    }
+    println("Lib Version: $libVersion")
+    return libVersion
 }
