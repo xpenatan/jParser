43000d42d30996987a054dfa3f8f4af370070c06
Natan <xpenatan@gmail.com>
2024-07-01 21:03:50 -0300

    update tool module

--- jParser/builder-tool/build.gradle.kts
+++ jParser/builder-tool/build.gradle.kts
@@ -2,10 +2,21 @@ plugins {
     id("java")
 }
 
+val moduleName = "jParser-build-tool"
+
 dependencies {
     implementation(project(":jParser:core"))
     implementation(project(":jParser:idl"))
     implementation(project(":jParser:teavm"))
     implementation(project(":jParser:cpp"))
     implementation(project(":jParser:builder"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = moduleName
+            from(components["java"])
+        }
+    }
 }
