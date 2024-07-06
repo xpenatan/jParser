a40bd70acbf4a7ee24959f075b9d4a06596224ea
Natan <xpenatan@gmail.com>
2024-07-06 20:35:42 -0300

    update loader lib

--- jParser/loader/loader-teavm/build.gradle.kts
+++ jParser/loader/loader-teavm/build.gradle.kts
@@ -6,8 +6,7 @@ val moduleName = "loader-teavm"
 
 dependencies {
     api("com.badlogicgames.gdx:gdx-jnigen-loader:${LibExt.jniGenVersion}")
-    implementation("org.teavm:teavm-core:${LibExt.teaVMVersion}")
-    implementation("com.github.xpenatan.gdx-teavm:backend-teavm:${LibExt.gdxTeaVMVersion}")
+    implementation("org.teavm:teavm-classlib:${LibExt.teaVMVersion}")
 }
 
 publishing {
