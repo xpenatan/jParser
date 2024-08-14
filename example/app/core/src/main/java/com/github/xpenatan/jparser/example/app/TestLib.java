293f5a89336309b72a95251e3e1045bb10cd15ce
Natan <xpenatan@gmail.com>
2024-08-14 15:09:42 -0300

    Prepare test

--- example/app/core/src/main/java/com/github/xpenatan/jparser/example/app/TestLib.java
+++ example/app/core/src/main/java/com/github/xpenatan/jparser/example/app/TestLib.java
@@ -3,7 +3,61 @@ package com.github.xpenatan.jparser.example.app;
 public class TestLib {
 
     public static boolean test() {
+        boolean constructorTest = testConstructorClass();
+        boolean attributeTest = testAttributeClass();
+        boolean staticAttributeTest = testStaticAttributeClass();
+        boolean attributeArrayTest = testAttributeArrayClass();
+        boolean methodTest = testMethodClass();
+        boolean staticMethodTest = testStaticMethodClass();
+        boolean callbackTest = testCallbackClass();
 
+        System.out.println("constructorTest: " + constructorTest);
+        System.out.println("attributeTest: " + attributeTest);
+        System.out.println("staticAttributeTest: " + staticAttributeTest);
+        System.out.println("attributeArrayTest: " + attributeArrayTest);
+        System.out.println("methodTest: " + methodTest);
+        System.out.println("staticMethodTest: " + staticMethodTest);
+        System.out.println("callbackTest: " + callbackTest);
+        return attributeTest && attributeArrayTest;
+    }
+
+    private static boolean testConstructorClass() {
+
+
+        return false;
+    }
+
+    private static boolean testAttributeClass() {
+
+
+        return false;
+    }
+
+    private static boolean testStaticAttributeClass() {
+
+
+        return false;
+    }
+
+    private static boolean testAttributeArrayClass() {
+
+
+        return false;
+    }
+
+    private static boolean testMethodClass() {
+
+
+        return false;
+    }
+
+    private static boolean testStaticMethodClass() {
+
+
+        return false;
+    }
+
+    private static boolean testCallbackClass() {
 
 
         return false;
