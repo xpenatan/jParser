067c8963c6270156f4484ea02a84268904aa81d2
Natan <xpenatan@gmail.com>
2024-08-16 10:05:07 -0300

    Add msvc debug option

--- jParser/builder/src/main/java/com/github/xpenatan/jparser/builder/targets/WindowsMSVCTarget.java
+++ jParser/builder/src/main/java/com/github/xpenatan/jparser/builder/targets/WindowsMSVCTarget.java
@@ -5,6 +5,10 @@ import com.github.xpenatan.jparser.builder.DefaultBuildTarget;
 
 public class WindowsMSVCTarget extends DefaultBuildTarget {
 
+    public static boolean DEBUG_BUILD;
+
+    // https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170
+
     public WindowsMSVCTarget() {
         this.libDirSuffix = "windows/vc/";
         this.tempBuildDir = "target/windows";
@@ -21,6 +25,13 @@ public class WindowsMSVCTarget extends DefaultBuildTarget {
         compilerOutputCommand = "-Fo:";
         cppFlags.add("-std:c++17");
         cppFlags.add("-c");
+        if(DEBUG_BUILD) {
+            cppFlags.add("/Z7"); // add debug information in .obj to work in visual studio
+            cppFlags.add("/Od");
+        }
+        else {
+            cppFlags.add("/O2");
+        }
         linkerOutputCommand = "/OUT:";
         libSuffix = "64.dll";
     }
@@ -35,6 +46,9 @@ public class WindowsMSVCTarget extends DefaultBuildTarget {
         }
         else {
             linkerCompiler.add("link");
+            if(DEBUG_BUILD) {
+                linkerCompiler.add("/DEBUG"); // Generates .pbd file
+            }
             linkerFlags.add("-DLL");
         }
         linkerCompiler.add("/NOLOGO");
