877320f2b0654c85d2b8db1d98be2ee33b743eb9
Natan <xpenatan@gmail.com>
2025-02-22 16:29:02 -0300

    Remove android target sdk

--- example/app/android/build.gradle.kts
+++ example/app/android/build.gradle.kts
@@ -12,7 +12,6 @@ android {
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
-        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
