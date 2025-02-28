09987a2f47e593f03057878786725ababec30a8a
Natan <xpenatan@gmail.com>
2025-02-28 08:23:45 -0300

    reduce android gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     val kotlinVersion = "2.1.10"
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
