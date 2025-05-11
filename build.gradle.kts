c83d98c4dd2c6ceacbb94cc36995217701593d00
Natan <xpenatan@gmail.com>
2025-05-11 15:15:23 -0300

    Remove old repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,6 @@ allprojects()  {
         google()
         mavenCentral()
         maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
-        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
         maven { url = uri("https://jitpack.io") }
         maven {
             url = uri("http://teavm.org/maven/repository/")
