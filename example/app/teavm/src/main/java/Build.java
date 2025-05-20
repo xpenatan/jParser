04ba01e2b303cf46bc1d99233458e069c7f01045
Natan <xpenatan@gmail.com>
2025-05-19 22:06:31 -0300

    Remove skip class

--- example/app/teavm/src/main/java/Build.java
+++ example/app/teavm/src/main/java/Build.java
@@ -1,12 +1,10 @@
 import com.github.xpenatan.gdx.backends.teavm.config.AssetFileHandle;
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration;
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder;
-import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass;
 import java.io.File;
 import java.io.IOException;
 import org.teavm.tooling.TeaVMTool;
 
-@SkipClass
 public class Build {
 
     public static void main(String[] args) throws IOException {
