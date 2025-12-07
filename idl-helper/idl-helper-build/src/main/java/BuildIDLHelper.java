f186911f1907973ddab4e194396777d5c35a5bad
Natan <xpenatan@gmail.com>
2025-12-07 19:39:11 -0300

    Add webgpu symbols

--- idl-helper/idl-helper-build/src/main/java/BuildIDLHelper.java
+++ idl-helper/idl-helper-build/src/main/java/BuildIDLHelper.java
@@ -172,6 +172,7 @@ public class BuildIDLHelper {
         linkTarget.linkerFlags.add("-Wl,--whole-archive");
         linkTarget.linkerFlags.add(libBuildCPPPath + "/libs/emscripten/" + op.libName + "_.a");
         linkTarget.linkerFlags.add("-Wl,--no-whole-archive");
+        linkTarget.linkerFlags.add("--use-port=emdawnwebgpu");
 //        linkTarget.linkerFlags.add("-Wl,--export-all");
         linkTarget.exportedFunctions.add("___stack_low");
         linkTarget.exportedFunctions.add("___stack_high");
