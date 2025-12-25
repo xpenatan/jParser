3a7ee336fb0e3f06582f4c54d43a622a7b4f20f4
Natan <xpenatan@gmail.com>
2025-12-25 15:30:16 -0300

    Add other platform visibility

--- examples/SharedLib/libA/lib-build/src/main/cpp/custom/LibACustomConfig.h
+++ examples/SharedLib/libA/lib-build/src/main/cpp/custom/LibACustomConfig.h
@@ -1,5 +1,9 @@
-#ifdef LIBA_EXPORTS
-#define LIBA_API __declspec(dllexport)
+#ifdef _WIN32
+    #ifdef LIBA_EXPORTS
+        #define LIBA_API __declspec(dllexport)
+    #else
+        #define LIBA_API __declspec(dllimport)
+    #endif
 #else
-#define LIBA_API __declspec(dllimport)
+    #define LIBA_API __attribute__((visibility("default")))
 #endif
