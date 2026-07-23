934c0cfb060feb7e2d4d9751a0af8f25b7314ed5
Natan <xpenatan@gmail.com>
2026-07-23 17:28:15 -0300

    Update project to toml

--- jParser/tools/gradle-plugin/build.gradle.kts
+++ jParser/tools/gradle-plugin/build.gradle.kts
@@ -68,8 +68,8 @@ publishing {
 
 easyPublishing {
     groupId.set(jParserGroup)
-    releaseVersion.set(libs.versions.jParserRelease)
-    snapshotVersion.set(libs.versions.jParserSnapshot)
+    releaseVersion.set(libs.versions.jParser)
+    snapshotVersion.set("-SNAPSHOT")
 
     snapshotRepositoryUrl.set("https://central.sonatype.com/repository/maven-snapshots/")
     releaseRepositoryUrl.set("https://central.sonatype.com")
